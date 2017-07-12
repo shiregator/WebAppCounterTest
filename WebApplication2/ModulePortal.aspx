8508cb534ebb2b3f361583d7770f13e8ac976110
shiregator <shiregators-2003@yahoo.com>
2017-07-12 04:01:55 +0000

    Initial Commit

---
+++ WebApplication2/ModulePortal.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ModulePortal.aspx.cs" Inherits="WebApplication2.ModulePortal" %>
+
+<!DOCTYPE html>
+  
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+  <meta charset=utf-8>
+  <title>Test Module</title>
+</head>
+
+<body>
+<form id="form2" runat="server">
+
+<asp:button runat="server" onclick="StartModule" type="button" Text="Start Module"></asp:button>
+<asp:button runat="server" onclick="AddModule" type="button" Text="Add Module"></asp:button>
+
+<p>Module Count: <%= countMsg %></p>
+
+</script> 
+
+</form>
+</body>
+  
+</html>
---
+++ WebApplication2/ModulePortal.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+	public partial class ModulePortal : Page
+	{
+		Module module;
+		protected String countMsg = "";
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			module = (Module)Session["module"];
+			if (module == null)
+			{
+				module = new Module();
+				Session["module"] = module;
+			}
+			System.Diagnostics.Debug.WriteLine("Page_Load() moduleCount = " + module.GetCount());
+		}
+
+		protected void StartModule(object sender, System.EventArgs e)
+		{
+			System.Diagnostics.Process p = System.Diagnostics.Process.Start("C:\\Utilities\\Module.exe");
+			if ((System.Diagnostics.Process)Session["moduleProcess"] == null)
+			{
+				Session["moduleProcess"] = p;
+			}
+			System.Diagnostics.Debug.WriteLine("ModulePortal StartModule() moduleCount = " + module.GetCount());
+			this.countMsg = "Added Module: moduleCount = " + module.GetCount();
+		}
+
+		protected void AddModule(object sender, System.EventArgs e)
+		{
+			module.AddModule();
+			System.Diagnostics.Debug.WriteLine("ModulePortal AddModule() moduleCount = " + module.GetCount());
+			this.countMsg = "Added Module: moduleCount = " + module.GetCount();
+		}
+	}
+}
